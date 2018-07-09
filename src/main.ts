beed46799d8b0cf845791267b57ed704549ca933
Piyush Chauhan <piyushchauhan2011@gmail.com>
2018-07-10 02:45:59 +0800

    gzip only in prod, disable console messages

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 console.log("Hello World")
 import("jschannel").then((Channel: any) => {
-  console.log(Channel.build)
+  // console.log(Channel.build)
 })
 import("evaporate").then(Evaporate => {
-  console.log(Evaporate)
+  // console.log(Evaporate)
 })
 import("a").then((a: any) => {
   console.log(a())
